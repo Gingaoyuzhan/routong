04423d1f8ac50fd85480083fc2d8355311f0cb0f
Gingaoyuzhan <gaoyuzhan37@gmail.com>
2026-01-13 18:26:09 +0800

    Initial Commit

---
+++ routong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  routong
+//
+//  Created by 高榆展 on 2026/1/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ routong/routongApp.swift
@@ -0,0 +1,17 @@
+//
+//  routongApp.swift
+//  routong
+//
+//  Created by 高榆展 on 2026/1/13.
+//
+
+import SwiftUI
+
+@main
+struct routongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
